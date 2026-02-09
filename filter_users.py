da179271550c272a7f4379bff57d6b9b6746b817
Posh <posh@Mac.fritz.box>
2026-02-09 17:24:45 +0100

    add filter by age

--- filter_users.py
+++ filter_users.py
@@ -4,18 +4,27 @@ import json
 def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
-
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+    for user in filtered_users:
+        print(user)
 
+
+def filter_users_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+    filtered_users = [user for user in users if user["age"] == age]
     for user in filtered_users:
         print(user)
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (name/age): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+        age_to_search = int(input("Enter an age to filter users: "))
+        filter_users_by_age(age_to_search)
     else:
         print("Filtering by that option is not yet supported.")
